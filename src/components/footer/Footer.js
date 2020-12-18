dd53c769d33878e49d72d51c0a3d88afaf825c01
christiansiagiann <christian.andreas@student.umn.ac.id>
2020-12-18 21:45:04 +0700

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import { AppBar, Toolbar, Switch, Typography, Button } from '@material-ui/core';
+import {Button} from '@material-ui/core';
 
 function Footer() {
   return (
